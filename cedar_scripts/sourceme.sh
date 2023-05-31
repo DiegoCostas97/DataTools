0b4b0bb8bd2718bae03f019e4c510a9bbddd36cb
Diego <diecosro@gmail.com>
2023-05-31 12:26:16 +0200

    Commit for clean repository only

--- cedar_scripts/sourceme.sh
+++ cedar_scripts/sourceme.sh
@@ -3,6 +3,7 @@ source /Users/diiego/software/ROOT/ROOT_6.24/install/bin/thisroot.sh
 #cd  /Users/diiego/software/GEANT4/GEANT4_10.1/install/bin
 #source geant4.sh
 #cd /Users/diiego/software/watchmal/DataTools/cedar_scripts
+
 export G4WORKDIR=/Users/diiego/software/GEANT4/GEANT4_10.1/build
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:+"$LD_LIBRARY_PATH:"}${G4LIB}/${G4SYSTEM}
 export WCSIMDIR=/Users/diiego/software/wcsim_replacement/build
